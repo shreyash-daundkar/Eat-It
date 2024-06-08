168d88a03a17119ab144621b301866c8e5326e25
Shreyash Daundkar <productiveappa@gmail.com>
2024-06-08 18:24:15 +0530

    Refactor

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,12 @@
 import "./Header.css"
 import CartNavigation from "./CartNavigation";
-import mealImage from "../../assets/meals.jpg"
 
 function Header() {
     return (
-        <>
         <header class="header">
             <h1>EAT IT</h1>
             <CartNavigation />
         </header>
-        <div class='main-image'>
-            <img src={mealImage}/>
-        </div>
-        </>
-
     );
 }
 
