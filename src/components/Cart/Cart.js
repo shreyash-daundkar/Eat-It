fbcc35d54e740de6139ed558efa8036cbdbc16e1
Shreyash Daundkar <productiveappa@gmail.com>
2024-06-10 13:01:05 +0530

    Added cart component

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,51 @@
+import Card from "../UI/Card";
+import CartItem from "./CartItem";
+import "./Cart.css"
+import Button from "../UI/Button";
+const meals = [
+    {
+        id: 1,
+        name: 'Misal',
+        price: 70,
+        quantity:1,
+    },
+    {
+        id: 2,
+        name: 'Misal',
+        price: 70,
+        quantity:1,
+    },
+    {
+        id: 3,
+        name: 'Misal',
+        price: 70,
+        quantity:1,
+    },
+    {
+        id: 4,
+        name: 'Misal',
+        price: 70,
+        quantity:1,
+    },
+]
+   
+
+function Cart() {
+    return (
+        <Card class='cart'>
+            <ul>
+                { meals.map(c => <CartItem cartItem={c} />) }
+            </ul>
+            <div class='total'>
+                <span>Total Amount</span>
+                <span>280</span>
+            </div>
+            <div class="action">
+                <Button class="sec-btn">Order</Button>
+                <Button>Close</Button>
+            </div>
+        </Card>
+    );
+}
+
+export default Cart;
