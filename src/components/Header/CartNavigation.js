53d4d00ee376b2f145441560706f1f67941addc2
Shreyash Daundkar <productiveappa@gmail.com>
2024-06-12 17:26:33 +0530

    Added open cart functionality

--- src/components/Header/CartNavigation.js
+++ src/components/Header/CartNavigation.js
@@ -1,10 +1,17 @@
 import "./CartNavigation.css"
 import CartIcon from "../Cart/CartIcon";
 import Button from "../UI/Button";
+import { useContext } from "react";
+import { cartContext } from "../../contexts/CartContextProvider";
 
 function CartNavigation() {
+
+  const { setCartModalOpen } = useContext(cartContext);
+  
+  const openCartModal = e => setCartModalOpen(true);
+
     return (
-        <Button>
+        <Button onClick={openCartModal}>
         <span class='icon'>
           <CartIcon />
         </span>
