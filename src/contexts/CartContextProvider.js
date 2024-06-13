ab707198d858858f99a6fe2f1de7e3770bcc8c83
Shreyash Daundkar <productiveappa@gmail.com>
2024-06-13 16:31:08 +0530

    Refactor cart context provider

--- src/contexts/CartContextProvider.js
+++ src/contexts/CartContextProvider.js
@@ -1,20 +1,64 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useReducer, useState } from "react";
+import cartReducer from "./cartReducer";
 
 export const cartContext = createContext({
     cartModalOpen: false,
     setCartModalOpen: () => {},
+    cartState: {},
+    addToCart: () => {},
+    incrementQuantity: () => {},
+    decrementQuantity: () => {},
 });
 
-
-
+               
 function CartContextProvider(props) {
-    
+
     const [ cartModalOpen, setCartModalOpen ] = useState(false);
 
+    let initialState = {
+        cartItems: [],
+        amount: 0,
+    }
+
+    const [state, dispatch] = useReducer(cartReducer, initialState);
+    
+    useEffect(() => {
+        let state = localStorage.getItem('cart-state');
+        state = state ? JSON.parse(state) : initialState;
+        dispatch({
+            type: 'lc',
+            payload: state,
+        });
+    }, []);
+
+    console.log(state)
+    
+    const addToCart = (meal, quantity) => dispatch({
+        type: 'add',
+        payload: {
+            meal,
+            quantity,
+        }
+    });
+        
+    const incrementQuantity = id => dispatch({
+        type: '++',
+        payload: id,
+    });
+            
+    const decrementQuantity = id => dispatch({
+        type: '--',
+        payload: id,
+    });
+        
     return (
         <cartContext.Provider value={{
             cartModalOpen: cartModalOpen,
             setCartModalOpen: setCartModalOpen,
+            cartState: state,
+            addToCart: addToCart,
+            incrementQuantity: incrementQuantity,
+            decrementQuantity: decrementQuantity,
         }}>
             {props.children}
         </cartContext.Provider>
